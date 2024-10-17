f1cf894287b99884bd576c7c7657b01daabc02d7
MikeV182 <michailvays@gmail.com>
2024-10-17 12:15:26 +0700

    Initialized the project

---
+++ build/CMakeFiles/LangtonsAnt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LangtonsAnt.dir/main.cpp.o"
+  "CMakeFiles/LangtonsAnt.dir/main.cpp.o.d"
+  "LangtonsAnt"
+  "LangtonsAnt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LangtonsAnt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
